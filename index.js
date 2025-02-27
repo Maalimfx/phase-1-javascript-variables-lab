c43f3151f80d793e09e1df23e7c87bd90ea4c957
abass <abass@DESKTOP-478KRS4.>
2025-02-27 15:41:23 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solution in this file!
+// Declare companyName as a const and assign 'Scuber'
+const companyName = 'Scuber';
+
+// Declare mostProfitableNeighborhood as a let and assign 'Chelsea'
+let mostProfitableNeighborhood = 'Chelsea';
+
+// Declare companyCeo as a let and assign 'Susan Smith'
+let companyCeo = 'Susan Smith';
